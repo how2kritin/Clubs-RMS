104953e220d480a6dfecde9dbf8b2f166bc03df1
Samyak Mishra <doo.di.diddle.doo@gmail.com>
2025-04-15 16:41:35 +0530

    [feat] add DB schema for calendar and interviews

---
+++ backend/models/calendar/interview_models.py
@@ -0,0 +1,91 @@
+import enum
+from sqlalchemy import Column, Integer, String, Enum, ForeignKey, Time, Date
+from sqlalchemy.orm import relationship
+from utils.database_utils import Base
+
+
+class InterviewSlot(Base):
+    __tablename__ = "interview_slot"
+
+    id = Column(Integer, primary_key=True, index=True)
+    start_time = Column(Time, nullable=False)
+    end_time = Column(Time, nullable=False)
+    date = Column(Date, nullable=False)
+
+    interview_schedule_id = Column(
+        Integer,
+        ForeignKey("interview_schedule.id"),
+        nullable=False,
+    )
+    interview_schedule = relationship(
+        "InterviewSchedule",
+        backref="interview_slot",
+    )
+
+    club_id = Column(
+        String,
+        ForeignKey("clubs.id"),
+        nullable=False,
+    )
+    club = relationship(
+        "Club",
+        backref="interview_slot",
+    )
+
+
+class InterviewPanel(Base):
+    __tablename__ = "interview_panel"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    interview_schedule_id = Column(
+        Integer,
+        ForeignKey("interview_schedule.id"),
+        nullable=False,
+    )
+    interview_schedule = relationship(
+        "InterviewSchedule",
+        backref="interview_panel",
+    )
+
+    club_id = Column(
+        String,
+        ForeignKey("clubs.id"),
+        nullable=False,
+    )
+    club = relationship(
+        "Club",
+        backref="interview_panel",
+    )
+
+    num_interviewers = Column(Integer, nullable=False)
+    # TODO: add interview names?
+
+
+class InterviewSchedule(Base):
+    __tablename__ = "interview_schedule"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    form_id = Column(
+        Integer,
+        ForeignKey("forms.id"),
+        nullable=False,
+    )
+    form = relationship(
+        "Form",
+        backref="interview_schedule",
+    )
+
+    # TODO: check with CC data/dummy data
+    club_id = Column(
+        String,
+        ForeignKey("clubs.id"),
+        nullable=False,
+    )
+    club = relationship(
+        "Club",
+        backref="interview_schedule",
+    )
+
+    slot_length = Column(Integer, nullable=False)  # in minutes
