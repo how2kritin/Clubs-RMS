237016aaeaeecba83e169f254da2c9903691f6b7
Samyak Mishra <doo.di.diddle.doo@gmail.com>
2025-04-16 12:01:18 +0530

    [fix] missing import, formatting

--- backend/main.py
+++ backend/main.py
@@ -5,23 +5,40 @@ from fastapi.middleware.cors import CORSMiddleware
 
 # just import whatever routers you want to import from ./routers here.
 
-from routers import recommendations_router, interviews_router, users_router, recruitment_router, clubs_router, applications_router
+from routers import (
+    recommendations_router,
+    interviews_router,
+    users_router,
+    recruitment_router,
+    clubs_router,
+    applications_router,
+    calendar_router,
+)
 from models.clubs.clubs_sync import sync_clubs
 from utils.database_utils import SessionLocal, init_db
 from utils.database_utils import reset_db, SessionLocal
 
 # FastAPI instance here, along with CORS middleware
 DEBUG = getenv("DEBUG_BACKEND", "False").lower() in ("true", "t", "1")
-app = FastAPI(debug=DEBUG, title="Recruitment Management System backend", description="Backend for the RMS-IIITH", )
-app.add_middleware(CORSMiddleware, allow_credentials=True, allow_origins=["*"], allow_headers=["*"],
-                   allow_methods=["GET", "POST"], )
+app = FastAPI(
+    debug=DEBUG,
+    title="Recruitment Management System backend",
+    description="Backend for the RMS-IIITH",
+)
+app.add_middleware(
+    CORSMiddleware,
+    allow_credentials=True,
+    allow_origins=["*"],
+    allow_headers=["*"],
+    allow_methods=["GET", "POST"],
+)
 
 
 # tasks to run on server startup.
 @app.on_event("startup")
 async def on_startup():
     # initialize the postgresql database.
-    reset_db() # TODO: check
+    reset_db()  # TODO: check
     db = SessionLocal()
 
     # sync clubs data from Clubs Council API
@@ -34,14 +51,24 @@ async def on_startup():
 # base path for checking if the backend is alive.
 @app.get("/", tags=["General"])
 async def index():
-    return {"message": "hello, you have reached the backend API service. what would you like to order?"}
+    return {
+        "message": "hello, you have reached the backend API service. what would you like to order?"
+    }
 
 
 # mount the imported routers on a path here.
 app.include_router(users_router.router, prefix="/api/user", tags=["User Management"])
 app.include_router(clubs_router.router, prefix="/api/club", tags=["Club Management"])
-app.include_router(applications_router.router, prefix="/api/application", tags=["Application Management"], )
-app.include_router(recruitment_router.router, prefix="/api/recruitment", tags=["Club Recruitment Management"], )
+app.include_router(
+    applications_router.router,
+    prefix="/api/application",
+    tags=["Application Management"],
+)
+app.include_router(
+    recruitment_router.router,
+    prefix="/api/recruitment",
+    tags=["Club Recruitment Management"],
+)
 app.include_router(
     recommendations_router.router,
     prefix="/api",
@@ -52,4 +79,8 @@ app.include_router(
     prefix="/api/interviews",
     tags=["Interview Scheduling"],
 )
-app.include_router(calendar_router.router, prefix="/api/calendar", tags=["Events Calendar"], )
+app.include_router(
+    calendar_router.router,
+    prefix="/api/calendar",
+    tags=["Events Calendar"],
+)
